d4b2fbc36094ed22dff6196c0f6fae936299c724
Arsenii2013 <a.nepein@g.nsu.ru>
2024-09-09 13:49:40 +0700

    Fix for emulate signed signal

--- AFE_emulator.c
+++ AFE_emulator.c
@@ -6,14 +6,14 @@ uint64_t timestamp = 0;
 uint32_t calibration = 0;
 
 int AFEEmulDDS_SYNC(void *){
-    AFERegs* regs = AFERegPtr();
+    volatile AFERegs* regs = AFERegPtr();
     if(regs->MFM.ctrl_reg & (1 << MFM_CTRL_OPERATION)){
         double arg = (double)timestamp / DDS_SYNC_PRD / 100.* 2 * M_PI;
-        double sinus = (sin(arg) + 1) / 2;
-        double cosinus = (cos(arg) + 1) / 2; 
+        double sinus = sin(arg);
+        double cosinus = cos(arg); 
         
-        uint64_t sinus_i = sinus * 0x8000000000000000;
-        uint64_t cosinus_i = cosinus * 0x8000000000000000;
+        int64_t sinus_i = sinus * 0x7fffffffffff0000;
+        int64_t cosinus_i = cosinus * 0x7fffffffffff0000;
 
         // TODO if(regs->MFM.mode == MFM_MODE_ANALOG_TO_ANALOG || regs->MFM.mode == MFM_MODE_ANALOG_TO_DIGITAL)
         regs->MFM.mf_ana_i_hi = sinus_i >> 32;
@@ -21,6 +21,7 @@ int AFEEmulDDS_SYNC(void *){
         // TODO else if(regs->MFM.mode == MFM_MODE_DIGITAL_TO_ANALOG || regs->MFM.mode == MFM_MODE_DIGITAL_TO_DIGITAL)
         regs->MFM.mf_dig_i_hi = cosinus_i >> 32;
         regs->MFM.mf_dig_i_low = cosinus_i;
+        timestamp += DDS_SYNC_PRD;
     }
 
     if(regs->MFM.ctrl_reg & (1 << MFM_CTRL_ZERO)){
@@ -36,6 +37,5 @@ int AFEEmulDDS_SYNC(void *){
     } else {
         regs->MFM.stat_reg |= (1 << MFM_STAT_CALIBRATION);
     }
-    timestamp += DDS_SYNC_PRD;
     return 0;
 }
